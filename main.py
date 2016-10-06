4011ea177d69879e9b5e15a5b7a8faeebc11a306
Garrett Beatty <gbt@localhost.localdomain>
2016-10-05 23:29:27 -0400

    added more functions for testing

--- main.py
+++ main.py
@@ -1,19 +1,25 @@
 import argparse
 import math
+import os
+
 
 #get command line arguments
 parser = argparse.ArgumentParser(description='LM')
-parser.add_argument('-c','--c', help='Corpus Files', required=True, nargs='+')
+parser.add_argument('-c', help='Corpus Folder', required=True, nargs='+')
+parser.add_argument('-g', help='Option to generate sentences', required=False, default=False)
 args = parser.parse_args()
 
-corpusFilesPath = args.c
+corpusFolders = args.c
+generateSentence = args.g
 
 corpuses = []
 
-#opens all the corpuses
-for path in  corpusFilesPath:
-	corpus = open(path, 'r')
-	corpuses.append(corpus)
+for folder in corpusFolders:
+	for dirpath, dirnames, filenames in os.walk(folder):
+		for filename in [f for f in filenames if f.endswith(".txt")]:
+			corpus = open(os.path.join(dirpath, filename), "r")
+			corpuses.append(corpus)
+
 
 N = 0
 
@@ -34,12 +40,42 @@ for corpus in corpuses: # for reach corpus
 		for word1, word2 in zip(words, words[1:]): #go through pairs
 			
 			if(counts[word1][1].get(word2) == None):
-				counts[word1][1][word2] = 1
-			else:
-				counts[word1][1][word2] += 1
+				counts[word1][1][word2] = 0
+			counts[word1][1][word2] += 1
 
-print(counts["apple"])
 
 #close files
 for corpus in corpuses:
 	corpus.close()
+
+probs = {}
+
+for word in counts:
+	if(probs.get(word) == None):
+		probs[word] = [0, {}]
+	probs[word][0] = math.log(counts[word][0] / N)
+
+	for word2 in counts[word][1]:
+		probs[word][1][word2] = math.log(counts[word][1][word2] / counts[word][0])
+
+def getProb(word1, word2 = None):
+	if(word2 == None):
+		return probs[word1][0]
+	else:
+		return probs[word1][1][word2]
+
+def getCount(word1, word2 = None):
+	if(word2 == None):
+		return counts[word1][0]
+	else:
+		return counts[word1][1][word2]
+
+def getHighestProbWordFromPrevWord(prevWord):
+	wordlist = probs[prevWord][1]
+	return max(wordlist, key=wordlist.get)
+
+def printPossibleWordsFromPrevWord(prevWord):
+	print(probs[prevWord][1])
+
+printPossibleWordsFromPrevWord("Alex")
+print(getHighestProbWordFromPrevWord("Alex"))
